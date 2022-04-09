ebffa4c7a162513ed9be3e8d11807e05280e9aab
Yam Borodetsky <yamyam263@gmail.com>
2022-04-09 22:17:51 +0300

    initial commit

---
+++ src/posts/posts.interface.ts
@@ -0,0 +1,15 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+// export interface Posts {}
+export class PostModel {
+  @ApiPropertyOptional({ type: Number })
+  id?: number;
+  @ApiProperty({ type: String, format: 'date-time' })
+  date: Date;
+  @ApiProperty({ type: String })
+  title: string;
+  @ApiProperty({ type: String })
+  body: string;
+  @ApiProperty({ type: String })
+  category: string;
+}
